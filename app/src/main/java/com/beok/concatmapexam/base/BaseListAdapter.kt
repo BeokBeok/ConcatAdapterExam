edfb80cc710b3025e8d8b565eada88b6fcbc7665
강현석 <kekemusa37@gmail.com>
2021-03-26 09:54:08 +0900

    [수정] BaseAdapter -> BaseListAdapter

--- app/src/main/java/com/beok/concatmapexam/base/BaseListAdapter.kt
+++ app/src/main/java/com/beok/concatmapexam/base/BaseListAdapter.kt
@@ -2,31 +2,29 @@ package com.beok.concatmapexam.base
 
 import android.view.ViewGroup
 import androidx.annotation.LayoutRes
+import androidx.databinding.ViewDataBinding
 import androidx.lifecycle.ViewModel
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 
-class BaseAdapter<T>(
+open class BaseListAdapter<T, VDB : ViewDataBinding>(
     @LayoutRes private val layoutResourceID: Int,
     private val bindingID: Int,
-    private val viewModels: Map<Int, ViewModel>,
+    private val viewModels: Map<Int, ViewModel> = mapOf(),
     diffUtil: DiffUtil.ItemCallback<T>
-) : ListAdapter<T, BaseViewHolder<T>>(diffUtil) {
+) : ListAdapter<T, BaseViewHolder<T, VDB>>(diffUtil) {
 
-    private val itemList = mutableListOf<T>()
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BaseViewHolder<T> =
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BaseViewHolder<T, VDB> =
         BaseViewHolder(
             parent = parent,
             layoutResourceID = layoutResourceID,
-            bindingID = bindingID
+            bindingID = bindingID,
+            viewModels = viewModels,
         )
 
-    override fun onBindViewHolder(holder: BaseViewHolder<T>, position: Int) {
-        holder.bind(item = itemList[position], viewModels = viewModels)
+    override fun onBindViewHolder(holder: BaseViewHolder<T, VDB>, position: Int) {
+        holder.bind(item = getItem(position))
     }
 
     override fun getItemViewType(position: Int): Int = layoutResourceID
-
-    override fun getItemCount(): Int = itemList.size
 }
