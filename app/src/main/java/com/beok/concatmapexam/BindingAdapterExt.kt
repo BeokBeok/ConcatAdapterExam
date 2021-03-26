81a01f87ac157e2a01fe1fd334373729ae72e46f
강현석 <kekemusa37@gmail.com>
2021-03-26 09:57:29 +0900

    [구현] srcWithGlide

---
+++ app/src/main/java/com/beok/concatmapexam/BindingAdapterExt.kt
@@ -0,0 +1,14 @@
+package com.beok.concatmapexam
+
+import android.widget.ImageView
+import androidx.databinding.BindingAdapter
+import com.bumptech.glide.Glide
+
+@BindingAdapter("srcWithGlide")
+fun srcWithGlide(imageView: ImageView, url: String?) {
+    if (url == null) return
+    Glide
+        .with(imageView)
+        .load(url)
+        .into(imageView)
+}
